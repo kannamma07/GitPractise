4ca5b85cb7f38a8e8ccd6d95d9a38ccb9f95fd05
kannamma07 <crystalkans94@gmail.com>
2021-06-30 21:51:18 +0530

    first git push

---
+++ src/org/cts/EmployeeDetails.java
@@ -0,0 +1,20 @@
+package org.cts;
+
+public class EmployeeDetails {
+
+	private void empName() {
+		System.out.println("kans");
+	}
+	private void empId() {
+		System.out.println("123");
+
+	}
+	public static void main(String[] args) {
+		EmployeeDetails e=new EmployeeDetails();
+		e.empId();
+		e.empName();
+	}
+
+}
+
+
