a4db9f119621f0123f007fbe8f8f2f10a5d5d60e
kannamma07 <crystalkans94@gmail.com>
2021-07-01 15:16:48 +0530

    git push

--- src/org/cts/EmployeeDetails.java
+++ src/org/cts/EmployeeDetails.java
@@ -7,12 +7,15 @@ public class EmployeeDetails {
 	}
 	private void empId() {
 		System.out.println("123");
-
+	}
+	private void empAge() {
+		System.out.println("32");
 	}
 	public static void main(String[] args) {
 		EmployeeDetails e=new EmployeeDetails();
 		e.empId();
 		e.empName();
+		e.empAge();
 	}
 
 }
